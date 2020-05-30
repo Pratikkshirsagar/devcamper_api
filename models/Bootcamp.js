2198b53f9d228f15505632dc93a041aa37b860b6
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-30 22:42:01 +0530

    geolocation added to db

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const moongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocode');
 
 const BootcampSchema = new moongoose.Schema({
   name: {
@@ -102,6 +103,25 @@ const BootcampSchema = new moongoose.Schema({
 // ** Create bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// ** GEOCODE &  create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // ** DO not save to address in Db
+  this.address = undefined;
   next();
 });
 
