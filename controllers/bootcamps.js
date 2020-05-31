842e912be48f24e67ed2b33aa9049402b2f6d2dc
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-31 13:28:25 +0530

    aip created for search bootcamp from dist

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocode');
 const Bootcamp = require('../models/Bootcamp');
 
 // * @ desc  Get All Bootcamps
@@ -68,4 +69,32 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: {} });
+});
+
+// * @desc  Get a bootcamp with a radius
+// * @route GET  /api/v1/bootcamps/radius/:zipcode/:distance
+// * @access Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // * Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // * Calc radius using radians
+  // * divide dist by radius of earth
+  // * Earth Radius = 3,963 mi / 6,378 Km
+
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
