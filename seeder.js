eb61deb20b7e17f6b92d2f857f38d113d5742b11
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-01 13:29:13 +0530

    seeder delete error resolve

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // * Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // * Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,11 +23,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // * Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-
+    await Course.create(courses);
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (error) {
@@ -38,7 +43,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
-
+    await Course.deleteMany();
     console.log('Data deleted...'.red.inverse);
     process.exit();
   } catch (error) {
@@ -48,6 +53,6 @@ const deleteData = async () => {
 
 if (process.argv[2] === '-i') {
   importData();
-} else if (process.argv === '-d') {
+} else if (process.argv[2] === '-d') {
   deleteData();
 }
